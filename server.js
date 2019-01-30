3d4814065278089b68e443dad00e34c75a2d4a1e
cruzayye <johnnyc010392@gmail.com>
2019-01-30 11:40:04 -0800

    set up env

---
+++ server.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+require('./lib/utils/connect')();
+
+const app = require('./lib/app');
+
+app.listen(7890, () => {
+  // eslint-disable-next-line no-console
+  console.log('running');
+});
