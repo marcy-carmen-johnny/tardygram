a3b3455f275ffb2ae7409a0f9743c91a499b2795
cruzayye <johnnyc010392@gmail.com>
2019-01-31 09:09:25 -0800

    still figuring out the post route

--- test/models/post.test.js
+++ test/models/post.test.js
@@ -9,36 +9,36 @@ const mongoose = require('mongoose');
 const User = require('../../lib/models/User');
 
 describe('Post model', () => {
-    const createPost = username => {
-        return createUser(username) 
-            .then(createUser => {
-                return request(app)
-                    .post('/auth/signin')
-                    .send({ username: createUser._id, password: 'password' });
-            })
-            .then(res => {
-                console.log('banana', res.body);
-                return res.body;
-            });
+    // const createPost = username => {
+    //     return createUser(username) 
+    //         .then(createUser => {
+    //             return request(app)
+    //                 .post('/auth/signin')
+    //                 .send({ username: createUser._id, password: 'password' });
+    //         })
+    //         .then(res => {
+    //             console.log('banana', res.body);
+    //             return res.body;
+    //         });
             
-    };
+    // };
 
-    const createUser = username => {
-        return User.create({
-            username, 
-            password: 'password'
-        })
-            .then(res =>  res.body);
-    };
-    beforeAll(() => {
-        connect();
-    });
-    beforeEach(done => {
-        mongoose.connection.dropDatabase(done);
-    });
-    afterAll(done => {
-        mongoose.connection.close(done);
-    });
+    // const createUser = username => {
+    //     return User.create({
+    //         username, 
+    //         password: 'password'
+    //     })
+    //         .then(res =>  res.body);
+    // };
+    // beforeAll(() => {
+    //     connect();
+    // });
+    // beforeEach(done => {
+    //     mongoose.connection.dropDatabase(done);
+    // });
+    // afterAll(done => {
+    //     mongoose.connection.close(done);
+    // });
     it('tests model for post', () => {
         return createPost('userrrrrr')
             .then(res => {
