dbcd63cd1f9e5fe30a412df78cc30cfedbb707dd
Marcy Silverman <marcysilve225@gmail.com>
2019-01-31 15:40:55 -0800

    changed server

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ require('dotenv').config();
 require('./lib/utils/connect')();
 
 const app = require('./lib/app');
+const PORT = process.env.PORT || 7890; 
 
-app.listen(7890, () => {
+app.listen(PORT, (a) => {
   // eslint-disable-next-line no-console
-  console.log('running');
+  console.log('running', PORT);
 });
